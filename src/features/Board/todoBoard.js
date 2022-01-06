942f831008bba72d5f783bc850642d661fcba1a0
ronklod <ronklod@gmail.com>
2022-01-06 15:01:05 +0200

    removing unused dependencies

--- src/features/Board/todoBoard.js
+++ src/features/Board/todoBoard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import '../todo.css';
 import TodoForm from "./todoForm";
 import TodosList from  "./todosList"
